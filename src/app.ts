f3f48a180196ad885ad80c8664561fbf640e5f99
Henri Vainio <matikkaeditorinkaantaja@gmail.com>
2025-04-09 14:37:39 +0300

    Object orientate

--- src/app.ts
+++ src/app.ts
@@ -1,104 +1,42 @@
 import {
     Scene,
-    PerspectiveCamera,
-    WebGLRenderer,
     BoxGeometry,
     Mesh,
     MeshPhongMaterial,
     DoubleSide,
-    AmbientLight,
-    PlaneGeometry,
-    MeshBasicMaterial,
 } from "three";
 
 
 import { CSG } from 'three-csg-ts';
 
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-
-const SCENE = new Scene();
+import Engine from "./engine";
 
 function main() {
-    const container = document.getElementById("viewer") as HTMLDivElement;
+    const elem = document.getElementById("viewer") as HTMLElement;
+    const engine = new Engine(elem, (scene) => {
+        const blueMaterial = new MeshPhongMaterial({ color: 0x3498db, side: DoubleSide });
+        const greenMaterial = new MeshPhongMaterial({ color: 0x1db32e, side: DoubleSide });
 
-    const camera = new PerspectiveCamera(75, container.clientWidth / container.clientHeight, 1, 5000);
-    const renderer = new WebGLRenderer({ antialias: true });
-    renderer.setSize(container.clientWidth, container.clientHeight);
-    container.appendChild(renderer.domElement);
+        const geometry = new BoxGeometry(200, 200, 100);
+        const cube = new Mesh(geometry, blueMaterial);
+        cube.position.set(0, 0, 0)
 
-    // Run code
-    const controls = addControls(renderer, camera);
-    addLighting()
-    animate();
-    render();
+        const geometry2 = new BoxGeometry(200, 200, 200);
+        const cube2 = new Mesh(geometry2, greenMaterial);
+        cube2.position.set(100, 100, 0)
 
-    document.getElementById("visualizeBtn")?.addEventListener("click", _ => render());
+        scene.add(cube)
+        scene.add(cube2)
 
-    function animate() {
-        requestAnimationFrame(animate);
-        controls.update();
-        renderer.render(SCENE, camera);
-    }
-}
-
-function render() {
-    clearScene();
-
-    const blueMaterial = new MeshPhongMaterial({ color: 0x3498db, side: DoubleSide });
-    const greenMaterial = new MeshPhongMaterial({ color: 0x1db32e, side: DoubleSide });
-
-    const geometry = new BoxGeometry(200, 200, 100);
-    const cube = new Mesh(geometry, blueMaterial);
-    cube.position.set(0, 0, 0)
-
-    const geometry2 = new BoxGeometry(200, 200, 200);
-    const cube2 = new Mesh(geometry2, greenMaterial);
-    cube2.position.set(100, 100, 0)
-
-    SCENE.add(cube)
-    SCENE.add(cube2)
-
-    const subtract = CSG.subtract(cube, cube2)
-    SCENE.add(subtract)
+        const subtract = CSG.subtract(cube, cube2)
+        scene.add(subtract)
+    });
+    engine.run();
 }
 
 
-function addControls(renderer: WebGLRenderer, camera: PerspectiveCamera) {
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.05;
-    camera.position.z = 600;
-    return controls;
-}
-
-function addFloor() {
-    const plane = new PlaneGeometry(100, 100);
-    const material = new MeshBasicMaterial({ color: 0x808080, side: DoubleSide })
-    const floor = new Mesh(plane, material);
-    floor.rotateX(-Math.PI / 2);
-    floor.rotateY(-1);
-    SCENE.add(floor);
-}
-
-function addLighting() {
-    const ambientLight = new AmbientLight(0xffffff, 0.5);
-    ambientLight.position.set(0, 1, 1).normalize();
-    SCENE.add(ambientLight);
-}
 
 
-// Clear old meshes
-function clearScene() {
-    while (SCENE.children.length > 0) {
-        const obj = SCENE.children[0] as any;
-        SCENE.remove(obj);
-
-
-        if (obj.geometry) obj.geometry.dispose();
-        if (obj.material) obj.material.dispose();
-    }
-    addLighting();
-}
 
 
 main();
--- src/engine.ts
+++ src/engine.ts
@@ -2,9 +2,9 @@ import {
     Scene,
     PerspectiveCamera,
     WebGLRenderer,
-    BoxGeometry,
+
     Mesh,
-    MeshPhongMaterial,
+
     DoubleSide,
     AmbientLight,
     PlaneGeometry,
@@ -12,7 +12,6 @@ import {
 } from "three";
 
 
-import { CSG } from 'three-csg-ts';
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
@@ -20,32 +19,38 @@ export default class Engine {
     container: HTMLElement;
     renderer: WebGLRenderer;
     camera: PerspectiveCamera;
+    scene: Scene;
+    renderFunc: (renderer: Scene) => void
 
-    constructor(container: HTMLElement) {
+    constructor(container: HTMLElement, renderFunc: (renderer: Scene) => void) {
         this.container = container;
         this.renderer = new WebGLRenderer({ antialias: true });
         this.camera = new PerspectiveCamera(75, container.clientWidth / container.clientHeight, 1, 5000);
+        this.scene = new Scene();
+        this.renderFunc = renderFunc;
     }
 
     run() {
+        const scene = this.scene;
+        const camera = this.camera;
         const container = this.container;
         const renderer = this.renderer;
-        const camera = new PerspectiveCamera(75, container.clientWidth / container.clientHeight, 1, 5000);
         renderer.setSize(container.clientWidth, container.clientHeight);
         container.appendChild(renderer.domElement);
 
         // Run code
-        const controls = addControls(camera);
-        addLighting()
+        const controls = this.addControls();
+        this.addLighting()
         animate();
-        render();
+        this.render();
 
         document.getElementById("visualizeBtn")?.addEventListener("click", _ => this.render());
+
 
         function animate() {
             requestAnimationFrame(animate);
             controls.update();
-            renderer.render(SCENE, camera);
+            renderer.render(scene, camera);
         }
     }
 
@@ -56,5 +61,41 @@ export default class Engine {
         controls.dampingFactor = 0.05;
         this.camera.position.z = 600;
         return controls;
+    }
+
+
+
+    addFloor() {
+        const plane = new PlaneGeometry(100, 100);
+        const material = new MeshBasicMaterial({ color: 0x808080, side: DoubleSide })
+        const floor = new Mesh(plane, material);
+        floor.rotateX(-Math.PI / 2);
+        floor.rotateY(-1);
+        this.scene.add(floor);
+    }
+
+    addLighting() {
+        const ambientLight = new AmbientLight(0xffffff, 0.5);
+        ambientLight.position.set(0, 1, 1).normalize();
+        this.scene.add(ambientLight);
+    }
+
+
+    // Clear old meshes
+    clearScene() {
+        while (this.scene.children.length > 0) {
+            const obj = this.scene.children[0] as any;
+            this.scene.remove(obj);
+
+
+            if (obj.geometry) obj.geometry.dispose();
+            if (obj.material) obj.material.dispose();
+        }
+        this.addLighting();
+    }
+
+    render() {
+        this.clearScene();
+        this.renderFunc(this.scene);
     }
 }
