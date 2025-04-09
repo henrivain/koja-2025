b0446420586f3904d573763718725c5f5c2ae6d6
Henri Vainio <matikkaeditorinkaantaja@gmail.com>
2025-04-09 17:02:38 +0300

    Add stl renderer

--- src/app.ts
+++ src/app.ts
@@ -8,21 +8,23 @@ import {
 
 } from "three";
 
-
+const GRAY = 0xbbbcbd
 
 import Engine from "./engine";
 import { Mover } from './Mover';
+import { STLLoader } from "three/examples/jsm/Addons.js";
 
 function main() {
     const blueMaterial = new MeshPhongMaterial({ color: 0x3498db, side: DoubleSide });
     const greenMaterial = new MeshPhongMaterial({ color: 0x1db32e, side: DoubleSide });
-    const redMaterial = new MeshPhongMaterial({ color: 0xb8333e, side: DoubleSide });
+    const lightBlue = new MeshPhongMaterial({ color: 0x3993ed, side: DoubleSide });
+
 
 
 
 
     const plane = new BoxGeometry(2000, 2000, 10);
-    const floor = new Mesh(plane, redMaterial);
+    const floor = new Mesh(plane, new MeshPhongMaterial({ color: GRAY, side: DoubleSide }));
     floor.position.set(0, -110, 0)
     floor.rotateX(-Math.PI / 2);
 
@@ -36,13 +38,21 @@ function main() {
     cube2.position.set(100, 100, 0)
 
     const geometry3 = new BoxGeometry(100, 100, 200);
-    const cube3 = new Mesh(geometry3, redMaterial);
+    const cube3 = new Mesh(geometry3, lightBlue);
     cube2.position.set(200, 300, 0)
 
     const elem = document.getElementById("viewport") as HTMLElement;
     const engine = new Engine(elem, [floor, cube, cube2]);
     engine.run();
     engine.addElem(cube3)
+
+
+    const loader = new STLLoader();
+    loader.load("Unnamed.stl", (geometry) => {
+        const lightBlue = new MeshBasicMaterial({ color: 0x3993ed, side: DoubleSide });
+        const mesh = new Mesh(geometry, lightBlue);
+        engine.addElem(mesh)
+    })
 
 
     new Mover(engine);
